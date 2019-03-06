10a1b485714512b0c7bd46229a0419a7f3cb2131
LichDragon <drago.ilya@yandex.ru>
2019-03-06 23:10:14 +0300

    СЛИЯНИЕ

--- app/src/main/java/by/backstagedevteam/safetraffic/MainActivity.java
+++ app/src/main/java/by/backstagedevteam/safetraffic/MainActivity.java
@@ -2,6 +2,7 @@ package by.backstagedevteam.safetraffic;
 
 import android.Manifest;
 import android.app.Notification;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.PointF;
@@ -12,7 +13,14 @@ import android.os.Bundle;
 
 import android.app.Activity;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -52,7 +60,7 @@ import com.yandex.runtime.network.RemoteError;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity implements UserLocationObjectListener, DrivingSession.DrivingRouteListener {
+public class MainActivity extends AppCompatActivity implements UserLocationObjectListener, DrivingSession.DrivingRouteListener,NavigationView.OnNavigationItemSelectedListener {
     private final String MAPKIT_API_KEY = "a574df9b-3431-4ff7-a6a9-2532869cfc80";
 
     //private final Point ROUTE_START_LOCATION = new Point(59.959194, 30.407094);
@@ -331,4 +339,89 @@ public class MainActivity extends Activity implements UserLocationObjectListener
     private void changeStatus(String status) {
         Toast.makeText(this, "Status" + status, Toast.LENGTH_SHORT).show();
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.geomap);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        switch (item.getItemId()){
+            case R.id.nav_manage:
+                Intent intent = new Intent (MainActivity.this, Settings.class);
+                startActivities(new Intent[]{intent});
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_manage) {
+
+
+        } else if (id == R.id.nav_rez)    {
+
+        } else if (id == R.id.nav_info)  {
+
+        } else if (id == R.id.nav_erorr)   {
+
+        } else if (id == R.id.nav_share)  {
+
+        } else if (id == R.id.nav_send)   {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+
+
 }
