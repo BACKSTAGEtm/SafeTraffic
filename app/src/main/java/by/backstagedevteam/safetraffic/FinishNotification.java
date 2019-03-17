684e3b9d88fe98e8726b016c07cfbdaa56beec8f
X-WL <xwolkodavx@mail.ru>
2019-03-17 21:57:13 +0300

    -don't used

---
+++ app/src/main/java/by/backstagedevteam/safetraffic/FinishNotification.java
@@ -0,0 +1,14 @@
+package by.backstagedevteam.safetraffic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FinishNotification extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_bar_main);
+    }
+}
